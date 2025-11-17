7bdd9d6283ffb7f77ddd20614552ba95fa00b631
Vaibhav Santhosh <vaibhavs.connect@gmail.com>
2025-11-18 00:43:02 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/academic-progress-dashboard/',
+  base: '/academic-progression-dashboard/',
 })
